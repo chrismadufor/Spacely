5fcd3b351c434cfbdf8c3283fde552b1ea17d2ff
Christian Madufor <chrismadufor@gmail.com>
2021-10-17 23:16:14 +0100

    Another commit

--- js/index.js
+++ js/index.js
@@ -21,40 +21,61 @@ window.onclick = function(event) {
     }
 }
 
-$('.owl-carousel').owlCarousel({
+$('.hero').owlCarousel({
     items: 1,
     loop:true,
     margin:10,
     nav:true,
     autoplay: true,
-    // responsive:{
-    //     0:{
-    //         items:1
-    //     },
-    //     600:{
-    //         items:2
-    //     },
-    //     1000:{
-    //         items:3
-    //     }
-    // }
 })
 
-// $('.owl-carousel').owlCarousel({
-//     items: 1,
-//     loop:true,
-//     margin:10,
-//     nav:true,
-//     autoplay: true,
-//     // responsive:{
-//     //     0:{
-//     //         items:1
-//     //     },
-//     //     600:{
-//     //         items:2
-//     //     },
-//     //     1000:{
-//     //         items:3
-//     //     }
-//     // }
-// })
+$('.testimonial-wrap').owlCarousel({
+    items: 1,
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay: true,
+})
+
+$('.type-wrap').owlCarousel({
+    items: 1,
+    loop:true,
+    margin:20,
+    nav:true,
+    autoplay: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        400:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+$('.popular-wrap').owlCarousel({
+    items: 1,
+    loop:true,
+    margin:20,
+    nav:true,
+    autoplay: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        500:{
+            items:2
+        },
+        1000:{
+            items:3
+        },
+        1150:{
+            items:4
+        }
+    }
+})
